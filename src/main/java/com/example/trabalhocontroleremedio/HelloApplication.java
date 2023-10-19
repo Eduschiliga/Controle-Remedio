867173937632124337dccdc56e83d453574144aa
Marcos Gomes <marcosartemio221@gmail.com>
2023-10-19 15:36:51 -0300

    Fix controller
    Alterado arquivo controler de CadastrarRemedioIdoso.fxml

--- src/main/java/com/example/trabalhocontroleremedio/HelloApplication.java
+++ src/main/java/com/example/trabalhocontroleremedio/HelloApplication.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("CadastrarRemedio.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("CadastrarRemedioIdoso.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Controle de Remédios");
         stage.setScene(scene);
