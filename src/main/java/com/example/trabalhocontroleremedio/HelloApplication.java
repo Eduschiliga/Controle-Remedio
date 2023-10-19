e514e7750d6ef54d619e8b5f7cdfc2461010b4f4
Eduardo Schiliga <eschiliga@alunos.utfpr.edu.br>
2023-10-19 11:46:59 -0300

    commit

--- src/main/java/com/example/trabalhocontroleremedio/HelloApplication.java
+++ src/main/java/com/example/trabalhocontroleremedio/HelloApplication.java
@@ -8,8 +8,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
+c    public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("CadastroUsuario.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Controle de Remédios");
