0c1adff28d1b6d51ab0c07c99e5c98d495abccff
Eduardo Schiliga <eschiliga@alunos.utfpr.edu.br>
2023-10-20 21:52:24 -0300

    commit

--- src/main/java/com/controleRemedio/application/CadastrarRemedioIdosoController.java
+++
@@ -1,5 +0,0 @@
-package com.controleRemedio.application;
-
-public class CadastrarRemedioIdosoController {
-    
-}
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.trabalhocontroleremedio {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.controleRemedio.application to javafx.fxml;
-    exports com.controleRemedio.application;
-}
