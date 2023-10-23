a501260e4664edaa22f5e3cc7706c7e278842f22
Marcos Gomes <marcosartemio221@gmail.com>
2023-10-23 19:26:40 -0300

    Atribuído Controladores
    Atribuído classes controladoras as telas

--- src/main/java/com/example/trabalhocontroleremedio/HelloController.java
+++
@@ -1,8 +0,0 @@
-package com.example.trabalhocontroleremedio;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
-public class HelloController {
-
-}
---
+++ src/main/java/com/example/trabalhocontroleremedio/LoginController.java
@@ -0,0 +1,20 @@
+package com.example.trabalhocontroleremedio;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class LoginController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
