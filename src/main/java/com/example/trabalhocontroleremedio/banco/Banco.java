2a2135971fd320e99716e0fa219989530d6ab51d
Marcos Gomes <marcosartemio221@gmail.com>
2023-12-03 02:37:50 -0300

    Maven
    Limpeza em dependências Maven

--- src/main/java/com/example/trabalhocontroleremedio/banco/Banco.java
+++ src/main/java/com/example/trabalhocontroleremedio/banco/Banco.java
@@ -4,22 +4,47 @@ import java.sql.*;
 
 public class Banco {
     public static void main(String[] args) {
-        //String url = "jdbc:sqlite:teste.db"; // Utilizar para testes
-        String url = "jdbc:sqlite:trabalhoControleRemedio.db"; // Banco padrão para o trabalho
+        String url = "jdbc:postgresql://localhost:5432/";
+        String user = "postgres";
+        String senha = "84627913";
+        String database = "teste"; //Utilizado para testes
+        //String database = "trabalhoIdosoRemedio";
         try{
-            Connection con = DriverManager.getConnection(url);
-            Statement stat = con.createStatement();
+            Connection con = null;
+            Statement stat = null;
+            try{
+                con = DriverManager.getConnection(url+database,user,senha);
+                stat = con.createStatement();
+            }catch(Exception Ex){
+                con = DriverManager.getConnection(url,user,senha);
+                stat = con.createStatement();
+                stat.executeUpdate("CREATE DATABASE " + database);
+                con.close();
+                con = DriverManager.getConnection(url+database,user,senha);
+                stat = con.createStatement();
+            }
+            
             // stat.executeUpdate("<SQL>"); // Funcionamento do comando
             /* Criação do banco */
-            stat.executeUpdate("CREATE TABLE Idoso(idIdoso INTEGER PRIMARY KEY AUTOINCREMENT, nascimento STRING, sexo STRING);");
-            stat.executeUpdate("CREATE TABLE Usuario(matricula INTEGER PRIMARY KEY, login STRING, senha STRING, tipo STRING, nome STRING, telefone STRING);");
-            stat.executeUpdate("CREATE TABLE Remedio(idRemedio INTEGER PRIMARY KEY AUTOINCREMENT, nome STRING, nome_generico STRING, categoria STRING);");
-            stat.executeUpdate("CREATE TABLE Horario(idHorario INTEGER PRIMARY KEY AUTOINCREMENT, h1 BOOLEAN, h2 BOOLEAN, h3 BOOLEAN, h4 BOOLEAN, h5 BOOLEAN," +
-                            " h6 BOOLEAN, h7 BOOLEAN, h8 BOOLEAN, h9 BOOLEAN, h10 BOOLEAN, h11 BOOLEAN, h12 BOOLEAN, h13 BOOLEAN, h14 BOOLEAN, h15 BOOLEAN, h16" + 
-                            " BOOLEAN, h17 BOOLEAN, h18 BOOLEAN, h19 BOOLEAN, h20 BOOLEAN, h21 BOOLEAN, h22 BOOLEAN, h23 BOOLEAN, h24 BOOLEAN);");
-            stat.executeUpdate("CREATE TABLE Remedio_Idoso(idRemedio_Idoso INTEGER PRIMARY KEY AUTOINCREMENT, dosagem REAL, forma_dosagem STRING, fk_horario" +
-                            " INTEGER, fk_idoso INTEGER, fk_remedio INTEGER, FOREIGN KEY(fk_horario) REFERENCES Horario(idHorario), FOREIGN KEY(fk_idoso)" +
-                            " REFERENCES Idoso(idIdoso), FOREIGN KEY(fk_remedio) REFERENCES Remedio(idRemedio));");
+            stat.executeUpdate("CREATE TABLE Idoso (idIdoso integer NOT NULL, nome character varying(255), nascimento character varying(255), " +
+                                "sexo character varying(255), CONSTRAINT Idoso_pkey PRIMARY KEY (idIdoso))");
+
+            stat.executeUpdate("CREATE TABLE Remedio (idRemedio integer NOT NULL, nome character varying(255), nome_generico character varying(255), " +
+                                "categoria character varying(255), forma_dosagem character varying(255), CONSTRAINT Remedio_pkey PRIMARY KEY (idRemedio))");
+
+            stat.executeUpdate("CREATE TABLE Horario (idhorario integer NOT NULL, h1 boolean, h2 boolean, h3 boolean, h4 boolean, " +
+                                "h5 boolean, h6 boolean, h7 boolean, h8 boolean, h9 boolean, h10 boolean, h11 boolean, h12 boolean, " +
+                                "h13 boolean, h14 boolean, h15 boolean, h16 boolean, h17 boolean, h18 boolean, h19 boolean, h20 boolean, " +
+                                "h21 boolean, h22 boolean, h23 boolean, h24 boolean, CONSTRAINT Horario_pkey PRIMARY KEY (idhorario))");
+                    
+            stat.executeUpdate("CREATE TABLE Usuario (matricula integer NOT NULL, login character varying(255), " +
+                                "senha character varying(255), tipo character varying(255), nome character varying(255), telefone character varying(255), " +
+                                "CONSTRAINT usuario_pkey PRIMARY KEY (matricula))");
+
+            stat.executeUpdate("CREATE TABLE Remedio_Idoso (idRemedio_Idoso integer NOT NULL, dosagem real, quantidade_dias integer, " +
+                                "fk_horario integer, fk_idoso integer, fk_remedio integer, CONSTRAINT Remedio_Idoso_pkey PRIMARY KEY (idRemedio_Idoso), " +
+                                "CONSTRAINT horario FOREIGN KEY (fk_horario) REFERENCES horario (idhorario), CONSTRAINT idoso FOREIGN KEY (fk_idoso) REFERENCES Idoso (idIdoso), " +
+                                "CONSTRAINT remedio FOREIGN KEY (fk_remedio) REFERENCES Remedio (idRemedio))");
 
             stat.executeUpdate("INSERT INTO Usuario VALUES (0,'admin','admin','Administrador',null,null);");
 
